eb78c943328f3a9a5f66763b43a947e235bc5c6e
Carter Ellis <54901754+CarterE1@users.noreply.github.com>
2023-05-20 01:01:08 -0400

    Update main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -16,7 +16,7 @@ fetch('../Files/words.txt').then((response) => response.text()).then(repo => {
     arrayOfLines = repo.split('\n');
 })
 
-fetch('../Files/combos.txt').then((response) => response.text()).then(repo => {
+fetch('../CarterE1/WordParty/blob/main/Files/combos.txt').then((response) => response.text()).then(repo => {
     arrayOfCombos = repo.split('\n');
 })
 
